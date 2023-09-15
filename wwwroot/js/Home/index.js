c675c2496269f646ab3359b476c26aafecee03b1
Carlos <carlos.dominguez@perezortiz.com.mx>
2023-09-14 17:25:49 -0700

    actualizacion de la vista

---
+++ wwwroot/js/Home/index.js
@@ -0,0 +1,13 @@
+function AddApp() {
+    console.log("Procesando informacion");
+    axios.post('/user', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
